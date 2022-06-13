b11a105991a8711240f0327f4c567dfd201cb337
José A. Fernández <aullasistemas@gmail.com>
2022-06-13 08:27:41 +0200

    0.82.1

--- quimeraps/__init__.py
+++ quimeraps/__init__.py
@@ -4,7 +4,8 @@ import os
 import sys
 
 DATA_DIR = os.path.join(
-    os.environ["ProgramFiles"] if sys.platform.startswith("win") else "/opt", "quimeraPS"
+    os.environ["ProgramFiles"] if sys.platform.startswith("win") else "/opt",
+    "quimeraPS",
 )
 
-__VERSION__ = "0.81.3"
+__VERSION__ = "0.82.1"
